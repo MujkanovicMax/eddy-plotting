2989424a13ceb4f56c1f1f1cd53861cf06f4dcb3
Mujkanovic.Max <mujkanovic.max@hotmail.de>
2019-04-26 16:34:30 +0200

    changed score calculation to normal exp functions

--- signal.py
+++ signal.py
@@ -26,15 +26,14 @@ data_dict["q"] = (data_arr[:,6]).astype(float)*1000
 data_dict["CO2"] = (data_arr[:,7]).astype(float)
 data_dict["p"] = (data_arr[:,8]).astype(float)
 
-
 tmp = np.zeros(data_dict["Ts_hcor"].shape)
 tmp2 = np.zeros(data_dict["q"].shape)
 
 
 mask_Ts = np.ones((500,))/500.
 mask_w  = np.ones((10,))/10.
-mask_q  = np.ones((100,))/100
-mask_CO2= np.ones((60,))/60
+mask_q  = np.ones((100,))/100.
+mask_CO2= np.ones((60,))/60.
 mask_dict = {"time":None,"u":None,"v":None,"w":mask_w,"Ts":None,"Ts_hcor":mask_Ts,"q":mask_q,"CO2":mask_CO2,"p":None}
 
 ger_mean_temp = 15 + 273.15  ###roughly the mean temperature of germany (yearly avg)
@@ -45,22 +44,26 @@ indoor_q = indoor_q*1000
 
 for j,var in enumerate(variables):
 
-
-    mean_dict[var] = np.convolve(data_dict[var],mask_dict[var] ,mode="same")
-    diff = np.abs(data_dict[var]-mean_dict[var])
-    w_dict[var] = 1-np.exp(-diff/np.nanstd(data_dict[var]))
-
+    
+        
+        mean_dict[var] = np.convolve(data_dict[var],mask_dict[var] ,mode="same")*1
+        diff = np.abs(data_dict[var]-mean_dict[var])
+        w_dict[var] = 1-np.exp(-diff/np.nanstd(data_dict[var]))
+        #w_dict[var] = w_dict[var]/np.nansum(w_dict[var]) * 10
 
     
+
+
 sigma = 15#np.nanstd(data_dict["Ts_hcor"]-indoor_temp)
 for i in range(len(mean_dict["Ts_hcor"])):
     if mean_dict["Ts_hcor"][i] > indoor_temp:
         
         deltaT = data_dict["Ts_hcor"][i] - indoor_temp
         if deltaT > 0:
-            tmp[i] = np.exp(-0.5*((data_dict["Ts_hcor"][i]-indoor_temp)/sigma)*((data_dict["Ts_hcor"][i]-indoor_temp)/sigma))
+            tmp[i] = np.exp(-deltaT/sigma)
         elif deltaT < 0:
             tmp[i] = 0
+            w_dict["Ts_hcor"][i] = 0
         else:
             print("wat")
             
@@ -68,23 +71,27 @@ for i in range(len(mean_dict["Ts_hcor"])):
         deltaT = data_dict["Ts_hcor"][i] - indoor_temp
         if deltaT > 0:
             tmp[i] = 0
+            w_dict["Ts_hcor"][i] = 0
         elif deltaT < 0:
-            tmp[i] = np.exp(-0.5*((data_dict["Ts_hcor"][i]-indoor_temp)/sigma)*((data_dict["Ts_hcor"][i]-indoor_temp)/sigma))
+            tmp[i] = np.exp(deltaT/sigma)
         else:
             print("wat")
             
     else:
         print("watwat")
 
-sigma = 1#np.nanstd(data_dict["q"]-indoor_q)
+#tmp = tmp/np.nansum(tmp) * 10
+
+sigma = 1.5#np.nanstd(data_dict["q"]-indoor_q)
 for i in range(len(mean_dict["q"])):
     if mean_dict["q"][i] > indoor_q:
         
         deltaq = data_dict["q"][i] - indoor_q
         if deltaq > 0:
-            tmp2[i] = np.exp(-0.5*((data_dict["q"][i]-indoor_q)/sigma)*((data_dict["q"][i]-indoor_q)/sigma))
+            tmp2[i] = np.exp(-deltaq/sigma)
         elif deltaq < 0:
             tmp2[i] = 0
+            w_dict["q"][i] = 0
         else:
             print("wat")
             
@@ -92,8 +99,9 @@ for i in range(len(mean_dict["q"])):
         deltaq = data_dict["q"][i] - indoor_q
         if deltaq > 0:
             tmp2[i] = 0
+            w_dict["q"][i] = 0
         elif deltaq < 0:
-            tmp2[i] = np.exp(-0.5*((data_dict["q"][i]-indoor_q)/sigma)*((data_dict["q"][i]-indoor_q)/sigma))
+            tmp2[i] = tmp2[i] = np.exp(deltaq/sigma)
         else:
             print("wat")
             
@@ -111,11 +119,12 @@ for i in range(len(sum_arr)):
         sum_arr[i] = 0
 
 
-index = np.where(sum_arr > 1.4)
+index = np.where(sum_arr >= 1.3)
 
-for var in variables:
+#for var in variables:
     
-    data_dict[var][index] = mean_dict[var][index]*1
+    #data_dict[var][index] = 0 ###mean_dict[var][index]*1
+
 
     
 name = ""
@@ -125,34 +134,35 @@ for i in variables:
 dt = np.linspace(0,len(data_dict["time"])-1,seperation)
 dt = np.round(dt).astype(int)
 
+
 fig,ax = plt.subplots(4,1, sharex=True,figsize=(100,40))
-    
-for i in range(seperation-1):
+test = np.zeros(data_dict["q"].shape)
+test[index] = 1
+for i in range(seperation-1):### default:seperation-1
     for j,var in enumerate(variables):
 
         interval = np.arange(dt[i], dt[i+1],1)
         ax[j].plot(interval,data_dict[var][dt[i]:dt[i+1]],linewidth=0.1,color = "k")
+        ax2 = ax[j].twinx()
+        
+        ax2.plot(interval,test[dt[i]:dt[i+1]],linewidth=0.5,color = "b")
+        ax2.set_ylim(0,2)
+        
         
         locs = np.arange(dt[i],dt[i+1], 4000)
         
         plt.xticks(locs, data_dict["time"][locs])
         plt.xlabel("Date / Time")
         ax[j].set_ylabel(var +" in "+ units[j])
-        ax[0].set_ylim(280.5,284.0)
-        ax[1].set_ylim(-2.,2.)
-        ax[2].set_ylim(3.15,3.40)
-        ax[3].set_ylim(411,419)
+        #ax[0].set_ylim(280.5,284.0)
+        #ax[1].set_ylim(-2.,2.)
+        #ax[2].set_ylim(3.15,3.40)
+        #ax[3].set_ylim(411,419)
+        #ax[j].set_ylim(np.nanquantile(data_dict[var][dt[i]:dt[i+1]],0.9),np.nanquantile(data_dict[var][dt[i]:dt[i+1]],1))
                                         ####maybe find a better solution than quantile limits
         plt.xlim(interval[0],interval[-1])
         
-        #ax2 = ax[j].twinx()
-        #if var == "Ts_hcor":
-            #ax2.plot(interval,w_dict[var][dt[i]:dt[i+1]]+tmp[dt[i]:dt[i+1]],linewidth=0.1,color = "b")
-        #elif var == "q":
-            #ax2.plot(interval,w_dict[var][dt[i]:dt[i+1]]+tmp2[dt[i]:dt[i+1]],linewidth=0.1,color = "b")
-        #else:
-            #ax2.plot(interval,w_dict[var][dt[i]:dt[i+1]],linewidth=0.1,color = "b")
-        #ax2.set_ylim(0,1)
+        
         
     plt.tight_layout()
     
